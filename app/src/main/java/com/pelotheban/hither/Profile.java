4a8168452c3574be015d6f317eb4ca3c6a639558
SamuelPuchala <sam@satov.ca>
2020-10-29 21:57:58 -0400

    starting to set up profile input

--- app/src/main/java/com/pelotheban/hither/Profile.java
+++ app/src/main/java/com/pelotheban/hither/Profile.java
@@ -8,16 +8,24 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class Profile extends AppCompatActivity {
+public class Profile extends AppCompatActivity implements View.OnFocusChangeListener {
+
+    private ImageView imgProfileImageX;
+    private EditText edtNameX, edtHitherX;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +35,24 @@ public class Profile extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        imgProfileImageX = findViewById(R.id.imgProfileImage);
+
+        edtNameX = findViewById(R.id.edtName);
+        edtNameX.setOnFocusChangeListener(this);
+        edtNameX.setImeOptions(EditorInfo.IME_ACTION_NEXT);
+        edtNameX.setRawInputType(InputType.TYPE_CLASS_TEXT);
+
+        edtHitherX = findViewById(R.id.edtHither);
+        edtNameX.setOnFocusChangeListener(this);
+        edtNameX.setImeOptions(EditorInfo.IME_ACTION_NEXT);
+        edtNameX.setRawInputType(InputType.TYPE_CLASS_TEXT);
+
+
+    }
+
+    @Override
+    public void onFocusChange(View view, boolean hasFocus) {
+
     }
 }
